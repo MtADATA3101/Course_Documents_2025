d9e0b291773e224527214069bda07982e0072a5b
Elizabeth Stregger <estregger@mta.ca>
2025-09-25 10:22:12 -0300

    ran styler

---
+++ data/dplyr_unstyled.r
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(nycflights13)
+
+flights_1 <- flights |>
+  group_by(month) |>
+  mutate(avg_monthly_delay = mean(dep_delay, na.rm = TRUE)) |>
+  ungroup()
+
+flights_2 <- flights_1 |>
+  mutate(avg_dep_time = mean(dep_time, na.rm = TRUE))
+
+flights_3 <- flights |>
+  mutate(avg_dep_time = mean(dep_time, na.rm = TRUE))
